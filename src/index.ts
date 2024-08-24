5ab233b62a054b6fcde1d6c9a790e116a5fcebe8
Max Cotton <maxcotton22@gmail.com>
2024-08-24 16:56:58 +0100

    feat: Setup basic express server

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,8 @@
-console.log("Hello World");
+import { app } from "./app";
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+	console.log(`Server is running on http://localhost:${PORT}`);
+});
 
